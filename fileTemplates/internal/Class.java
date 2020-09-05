2c10c0efe5017de3b9437022bc76cb9a207ac22e
莫须 <nagile.zt@alibaba-inc.com>
2020-09-05 14:24:04 +0800

    IU-2020.1.2 <nagile@B-T2JNMD6R-1902.local Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, equalsHashCodeTemplates.xml, lombok-plugin.xml, yiiguxing.translation.xml, Default.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, remote-servers.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, KeyPromoterXSettings.xml, notifications.xml, _@user_Darcula.icls, path.macros.xml, databaseDrivers.xml, mavenVersion.xml, Default for macOS copy.xml, consoleFolding.xml, Default.xml, docker.xml, Class.java, jdk.table.xml, smartfox_info.xml, _@user_Monokai Pro.icls, _@user_Arc Dark.icls, laf.xml, proxy.settings.xml, codota.xml, tala_settings.xml, Alibaba-CodeStyle.xml, grazie_global.xml, toStringTemplates.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author nagile_zhou
+ * @date Created by moxu at ${DATE}
+ * Don't be afraid, there will be no bugs this time
+ */
+public class ${NAME} {
+}
